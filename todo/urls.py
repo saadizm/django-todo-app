1fa6b9dbe6ee6679901a2eb5c425720e5d66c03a
Saad Ahmed <36796193+saadizm@users.noreply.github.com>
2025-09-11 16:08:31 +0500

    Deleting files and adding some styling to the html

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
 from todo import views
-
+app_name = "todo" 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("add/", views.add_todo, name='add_todo'),
+    path("list/", views.list_todos, name="list_todos")
+
 ]
--- todo/views.py
+++ todo/views.py
@@ -5,3 +5,14 @@ from django.http import HttpResponse
 def index(request):
     context = {'message': "Application allows user to save their todos.."}
     return render(request, "todo/index.html", context)
+
+def add_todo(request):
+    context = {"message": "Add your todo here.."}
+    return render(request, "todo/add_todo.html", context)
+
+def process_data(request):
+    pass
+
+def list_todos(request):
+    context = {"message": "Your created todos..."}
+    return render(request, "todo/list_todos.html", context)
