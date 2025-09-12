a60975755c6fc61af8b9d2aca3d5bbdf9b9b1bb5
Saad Ahmed <36796193+saadizm@users.noreply.github.com>
2025-09-12 19:58:38 +0500

    Added data into a todo model

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from todo.models import Todo
+from datetime import datetime
 # Create your views here.
 
 def index(request):
@@ -15,10 +17,20 @@ def process_data(request):
     if request.method == 'POST':
         todo_title = request.POST.get("title")
         todo_description = request.POST.get("description")
+        is_completed = False if request.POST.get("option_group") == 'no' else True
+        created_at = datetime.now()
+        updated_at = datetime.now()
+        due_date = request.POST.get("dueDate")
+        priority = request.POST.get('priority')
+        todo_item = Todo(title = todo_title, description = todo_description, is_completed =is_completed,
+                         created_at = created_at, updated_at = updated_at, due_date = due_date, priority = priority)
+        todo_item.save()
+        return HttpResponseRedirect(reverse("todo:list_todos"))
+    else:
         pass
-    pass
+        
 
 def list_todos(request):
     all_todos = Todo.objects.all()
-    context = {"objects": all_todos}
+    context = {"all_todos": all_todos}
     return render(request, "todo/list_todos.html", context)
